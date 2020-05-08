7dadf6aff46a95006478f805d0c7538e07492d46
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-05-08 09:55:57 +0100

    upToTop.js file added, css starter code included in text.html.

--- scripts/text.js
+++ scripts/text.js
@@ -16,6 +16,7 @@ function resetExample_1(){
   changeSample.setAttribute("class", "sample_Content");
 }
 // -------------------------------------------------------------------------------
+
 // Text Example 2: font-weight
 
 document.querySelector("#text_btn_2").addEventListener( "click", textExample_2);
@@ -31,5 +32,38 @@ function resetExample_2(){
   let changeSample = document.querySelector("#textSample_2");
   changeSample.setAttribute("class", "sample_Content");
 }
+// -------------------------------------------------------------------------------
 
+// Text Example 3: color
+
+document.querySelector("#text_btn_3").addEventListener( "click", textExample_3);
+
+function textExample_3(){
+  let changeSample = document.querySelector("#textSample_3");
+  changeSample.setAttribute("class", "textChange_3");
+}
+
+document.querySelector("#reset_btn_3").addEventListener( "click", resetExample_3);
+
+function resetExample_3(){
+  let changeSample = document.querySelector("#textSample_3");
+  changeSample.setAttribute("class", "sample_Content");
+}
+// -------------------------------------------------------------------------------
+
+// Text Example 4: text-align
+
+document.querySelector("#text_btn_4").addEventListener( "click", textExample_4);
+
+function textExample_4(){
+  let changeSample = document.querySelector("#textSample_4");
+  changeSample.setAttribute("class", "textChange_4");
+}
+
+document.querySelector("#reset_btn_4").addEventListener( "click", resetExample_4);
+
+function resetExample_4(){
+  let changeSample = document.querySelector("#textSample_4");
+  changeSample.setAttribute("class", "sample_Content");
+}
 // -------------------------------------------------------------------------------
