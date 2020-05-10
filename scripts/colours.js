02000be56c5a76a6352b4223674665b39fb26c60
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-05-10 22:05:57 +0100

    Colours topic files started.

---
+++ scripts/colours.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function changeSampleContent(n, content_id, cssChangeClass, cssChangeHelper, originalCSS ) {
+
+  document.querySelector(`#change_btn_${n}`).addEventListener( `click`, changeExample);
+
+  function changeExample(){
+    let changeSample = document.querySelector(`#${content_id}${n}`);
+    changeSample.setAttribute(`class`, `${cssChangeClass}${n } ${cssChangeHelper}`);
+  }
+
+  document.querySelector(`#reset_btn_${n}`).addEventListener( `click`, resetExample);
+
+  function resetExample(){
+    let changeSample = document.querySelector(`#${content_id}${n}`);
+    changeSample.setAttribute(`class`, `${originalCSS}`);
+  }
+}
+// --------------------------------------------------------------------------------
+
+// Colours Example 1: Named
+changeSampleContent("1", "colourID_", "colourChange_", "colourChangeHelper", "colour_Original");
