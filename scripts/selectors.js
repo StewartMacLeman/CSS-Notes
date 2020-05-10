c19910fd3bd3ae79adf4d95b6313ede6c15fdeec
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-05-10 19:45:04 +0100

    Selectors' content finished. Nav borders adjusted.

--- scripts/selectors.js
+++ scripts/selectors.js
@@ -23,3 +23,51 @@ changeSampleContent("1", "selectorID_", "selectorChange_", "selectorChangeHelper
 
 // Selectors Example 2: Combinators - Descendant (space)
 changeSampleContent("2", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 3: Combinators - Child (>)
+changeSampleContent("3", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 4: Combinators - Adjacent Sibling (+)
+changeSampleContent("4", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 5: Combinators - General Sibling (~)
+changeSampleContent("5", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 6: Pseudo-classes (:first-child)
+changeSampleContent("6", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 7: Pseudo-classes (:last-child)
+changeSampleContent("7", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 8: Pseudo-classes (:nth-child(n))
+changeSampleContent("8", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 9: Pseudo-elements (::first-letter)
+changeSampleContent("9", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 10: Pseudo-elements (::first-line)
+changeSampleContent("10", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 11: Pseudo-elements (::before)
+changeSampleContent("11", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 12: Pseudo-elements (::after)
+changeSampleContent("12", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 13: Attributes [attribute]
+changeSampleContent("13", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 14: Attributes [attribute=]
+changeSampleContent("14", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 15: Attributes [attribute~="value"]
+changeSampleContent("15", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 16: Attributes [attribute^="value"]
+changeSampleContent("16", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 17: Attributes [attribute$="value"]
+changeSampleContent("17", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
+
+// Selectors Example 18: Attributes [attribute*="value"]
+changeSampleContent("18", "selectorID_", "selectorChange_", "selectorChangeHelper", "selector_Original");
